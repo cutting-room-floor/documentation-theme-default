a7344b02e89695992bb6562e1197c14d3ae876ab
Tom MacWright <tom@macwright.org>
2015-10-30 15:32:28 -0400

    Simplify design, upgrade basscss

--- assets/site.js
+++ assets/site.js
@@ -2,17 +2,19 @@
 
 // add anchor links to headers
 anchors.options.placement = 'left';
-anchors.add();
+anchors.add().remove('.no-anchor');
 
 // Filter UI
 var tocElements = document.getElementById('toc').getElementsByTagName('a');
 document.getElementById('filter-input').addEventListener('keyup', function(e) {
 
+  var i, element;
+
   // enter key
   if (e.keyCode === 13) {
     // go to the first displayed item in the toc
-    for (var i = 0; i < tocElements.length; i++) {
-      var element = tocElements[i];
+    for (i = 0; i < tocElements.length; i++) {
+      element = tocElements[i];
       if (!element.classList.contains('hide')) {
         location.replace(element.href);
         return e.preventDefault();
@@ -27,8 +29,8 @@ document.getElementById('filter-input').addEventListener('keyup', function(e) {
     match = function(text) { return text.toLowerCase().indexOf(value) !== -1; };
   }
 
-  for (var i = 0; i < tocElements.length; i++) {
-    var element = tocElements[i];
+  for (i = 0; i < tocElements.length; i++) {
+    element = tocElements[i];
     if (match(element.innerHTML)) {
       element.classList.remove('hide');
     } else {
